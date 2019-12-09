5d848fab6cfd3b302cf1e93172c534944195fa48
S-Toky <stokyr29@gmail.com>
2019-12-09 15:26:11 +0300

    add name devise

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,11 @@ ActiveRecord::Schema.define(version: 2019_12_09_091001) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
