dd50a80d4674028de23da15c2383d10cbf3f94db
R David N H <aafano19@gmail.com>
2020-01-10 06:31:25 +0300

    col

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,19 @@
 class HomesController < ApplicationController
   def index
   	@actualite = Actuality.all
+  	@candidat = Candidat.all
+  	@suivi1 = 0 
+    @suivi2 = 0 
+    @suivi3 = 0 
+    @total = 0
+  	@candidat.each do |candidat|
+  		candidat.promesses.each do |promesses|
+	        @total += promesses.suivis.count 
+	        @suivi = Suivi.where(promess: promesses)
+	        @suivi1 += (@suivi.where(start: true)).count
+	        @suivi2 += ((@suivi.where(transition: true).count))
+	        @suivi3 += ((@suivi.where(finished: true).count) ) 
+      	end 
+    end   
   end
 end
