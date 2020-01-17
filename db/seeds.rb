1cdca718be0d45720fba5989bad9b63661f90138
fanomallot <aafano19@gmail.com>
2020-01-17 06:56:53 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.destroy_all
+User.destroy_all
 Suivi.destroy_all
 Survey.destroy_all
 Comment.destroy_all
