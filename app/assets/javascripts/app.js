01b26270710038eee50046093167ef900a04a031
tsiryjl <tsiry.razanakoto@gmail.com>
2020-08-08 15:14:23 +0300

    ergoJS

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,29 +1,8 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-
-
 //= require_self
 
 //= require assets/js/jquery-3.2.1.min
 //= require assets/js/popper.min
 //= require assets/js/bootstrap.min
-//= require revolution/js/jquery.themepunch.tools.min
-//= require revolution/js/jquery.themepunch.revolution.min
-//= require revolution/js/extensions/revolution.extension.actions.min
-//= require revolution/js/extensions/revolution.extension.video.min
-//= require revolution/js/extensions/revolution.extension.slideanims.min
-//= require revolution/js/extensions/revolution.extension.layeranimation.min
-//= require revolution/js/extensions/revolution.extension.navigation.min
-//= require revolution/js/extensions/revolution.extension.slideanims.min
 //= require assets/js/counterup/jquery.waypoints.min
 //= require assets/js/counterup/jquery.counterup.min
 //= require assets/js/counterup/apear
@@ -34,8 +13,6 @@
 //= require circle-bar/plugins
 //= require assets/js/circle-active
 
-
-//= require assets/js/smoothscroll
 //= require assets/js/theme
 
 
