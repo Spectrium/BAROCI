9d90ad5ca5eb83b2981114d96b0f1c95b08cf13d
tsiryjl <tsiry.razanakoto@gmail.com>
2020-08-11 16:25:25 +0300

    NavBar ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,17 @@
 //= require turbolinks
 //= require_tree .
 
+// Get the container element
+var btnContainer = document.getElementById("navigation");
+
+// Get all buttons with class="btn" inside the container
+var btns = document.getElementsByClassName("Nbr");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
