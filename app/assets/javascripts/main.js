c96dce92cf1d1053929a2ea86338995c91f636e0
tsiryjl <tsiry.razanakoto@gmail.com>
2020-08-12 21:36:23 +0300

    out

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -333,28 +333,28 @@ dots:false,
 //========================
 //CountDown
 //========================
-// function count(){
-//     const second = 1000,
-//     minute = second * 60,
-//     hour = minute * 60,
-//     day = hour * 24;
-//     let countDown = new Date('Sept 01, 2020 00:00:00').getTime(),
-//     x = setInterval(function() {    
+function count(){
+    const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+    let countDown = new Date('Sept 01, 2020 00:00:00').getTime(),
+    x = setInterval(function() {    
 
-//     let now = new Date().getTime(),
-//       distance = countDown - now;
+    let now = new Date().getTime(),
+      distance = countDown - now;
 
-//     document.getElementById('days').innerText = Math.floor(distance / (day)),
-//     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-//     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-//     document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+    document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+    document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-//     //do something later when date is reached
-//     //if (distance < 0) {
-//     //  clearInterval(x);
-//     //  'IT'S MY BIRTHDAY!;
-//     //}
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  'IT'S MY BIRTHDAY!;
+    //}
 
-//     }, second)
-// }
+    }, second)
+}
 
